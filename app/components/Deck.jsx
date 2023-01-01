9fe221f698a2a57d4e9d63f2f9e502e217d2c5e3
ivinogradov <ilya.vngrdv@gmail.com>
2023-01-01 15:51:20 -0600

    Load 5 cards from the deck

--- app/components/Deck.jsx
+++ app/components/Deck.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import  { View, Text } from 'react-native';
+import  { View, Text, FlatList } from 'react-native';
 import Card from './Card';
-import { NEW_DECK_URL } from '../urls';
+import { NEW_DECK_URL, getDrawCardsURL } from '../urls';
+
+const NUMBER_OF_CARDS_TO_DRAW = 5;
 
 const Deck = () => {
-    const [deckId, setDeckId] = useState(null);
+    const [deckId, setDeckId] = useState();
+    const [cards, setCards] = useState([]);
 
     // call once on component mount
     useEffect(() => {
@@ -22,10 +25,34 @@ const Deck = () => {
         loadNewDeck(setDeckId);
     }, []);
 
+    // call every time for each deckId update
+    useEffect(() => {
+        const drawCards = async(callback) => {
+            if (!deckId) {
+                return;
+            }
+            const response = await fetch(getDrawCardsURL(deckId, NUMBER_OF_CARDS_TO_DRAW));
+            const jsonObject = await response.json();
+            if (jsonObject && jsonObject.cards) {
+                callback(jsonObject.cards);
+            }
+        }
+        drawCards(setCards);
+    }, [deckId]);
+
     return(
         <View>
             <Text>You've got a deck:{deckId}</Text>
             <Card />
+            <FlatList
+                data={cards}
+                renderItem={({item, index})=>(
+                    <Text>{item.code}</Text>
+                )}
+                keyExtractor={(item, index)=> {
+                    item.code.toString();
+                }}
+            />
         </View>
     );
 }
--- app/urls.js
+++ app/urls.js
@@ -1,1 +1,5 @@
 export const NEW_DECK_URL = 'https://deckofcardsapi.com/api/deck/new/';
+
+export const getDrawCardsURL = (deckId, numberOfCards) => {
+    return `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${numberOfCards}`;
+}
